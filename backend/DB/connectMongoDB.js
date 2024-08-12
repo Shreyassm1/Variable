07670d60a06e557ceb8c6d38e4eeb7f1e59042a6
Shreyassm1 <shreyasmishrasm048@gmail.com>
2024-08-12 18:00:06 +0530

    Connect to DB

---
+++ backend/DB/connectMongoDB.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectMongoDB = async() => {
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log("DB Connected");
+    }
+    catch(error){
+        console.error(`Error connection to MongoDB: ${error.message}`);
+        process.exit(1);
+    }
+}
+
+export default connectMongoDB;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import connectMongoDB from "./DB/connectMongoDB.js";
 
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth",authRoutes);
 
-app.listen(8000, ()=>{
-    console.log("server is running on port 8000.");
+app.listen(PORT, ()=>{
+    console.log(`Server is running on ${PORT}`);
+    connectMongoDB();
 })
