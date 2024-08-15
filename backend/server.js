d27b19e495bfa089503a90a459a546073384ff6b
Shreyassm1 <shreyasmishrasm048@gmail.com>
2024-08-15 11:03:27 +0530

    Implement posting routes/controllers

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
+
 import connectMongoDB from "./DB/connectMongoDB.js";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
@@ -23,6 +26,7 @@ app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 app.use("/api/auth",userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on ${PORT}`);
