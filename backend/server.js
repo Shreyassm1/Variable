5bb690c30f9bc24df83249ee9f68feee1a3dd1cf
Shreyassm1 <shreyasmishrasm048@gmail.com>
2024-08-15 13:20:19 +0530

    Implement notification routes/controllers

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import postRoutes from "./routes/post.routes.js";
+import notificationRoutes from "./routes/notification.routes.js";
 
 import connectMongoDB from "./DB/connectMongoDB.js";
 import cookieParser from "cookie-parser";
@@ -24,9 +25,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
-app.use("/api/auth",authRoutes);
-app.use("/api/auth",userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/auth", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on ${PORT}`);
